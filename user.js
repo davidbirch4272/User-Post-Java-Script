6c79f1d02a46c9b9d33e4bb8834138622790d193
davidbirch4272 <davidbirch4272@gmail.com>
2025-04-09 10:49:01 -0400

    Additions

--- user.js
+++ user.js
@@ -1,1 +1,28 @@
-console.log(localStorage.getItem("id"));
+const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+const id = event.target.value;
+renderposts(id);
+}
+
+async function renderposts(id) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');  
+}
+    
+    function postHTML(post) {
+    return `
+        <div class="post">
+        <div class="post__title">
+        ${post.title}
+        </div>
+        <p class="post__body">
+        ${post.body}
+        </p>
+        </div>
+        `
+    }   
+
+    renderposts(id);
